02db4fd922fd1ce648a52fcc44421d4742e89915
akik-hossain <akikhs00@gmail.com>
2023-11-11 01:10:07 +0600

    styled sidebar

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -2,12 +2,13 @@
 
     {{-- Dashboard --}}
     <ul class="list-unstyled">
-        <li class="sidebar-list-item border"><a class="sidebar-link text-muted active" href="{{ url('/') }}">
+        <li class="sidebar-list-item border-top border-bottom"><a class="sidebar-link text-muted active"
+                href="{{ url('/') }}">
                 <span class="sidebar-link-title fs-5">Dashboards </span></a>
         </li>
 
         {{-- Employees --}}
-        <li class="sidebar-list-item py-2 border"><a class="sidebar-link text-muted " href="#"
+        <li class="sidebar-list-item py-2 border-top border-bottom"><a class="sidebar-link text-muted " href="#"
                 data-bs-target="#cmsDropdown" role="button" aria-expanded="false" data-bs-toggle="collapse"><i
                     class="fa-solid fa-user-group me-2 "></i>
                 <span class="sidebar-link-title fs-5">Employees</span></a>
@@ -40,13 +41,13 @@
         </li>
 
         {{-- Task --}}
-        <li class="sidebar-list-item py-2 border"><a class="sidebar-link text-muted " href="#"><i
-                    class="fa-solid fa-list-check me-2"></i>
+        <li class="sidebar-list-item py-2 border-top border-bottom"><a class="sidebar-link text-muted "
+                href="#"><i class="fa-solid fa-list-check me-2"></i>
                 <span class="sidebar-link-title fs-5">Task</span></a>
         </li>
 
         {{-- Manage Leave --}}
-        <li class="sidebar-list-item py-2 border"><a class="sidebar-link text-muted " href="#"
+        <li class="sidebar-list-item py-2 border-top border-bottom"><a class="sidebar-link text-muted " href="#"
                 data-bs-target="#componentsDropdown" role="button" aria-expanded="false" data-bs-toggle="collapse"><i
                     class="fa-solid fa-person-walking-arrow-right me-2"></i>
                 <span class="sidebar-link-title fs-5">Leave</span></a>
@@ -63,19 +64,19 @@
         </li>
 
         {{-- Manage Tour --}}
-        <li class="sidebar-list-item py-2 border"><a class="sidebar-link text-muted " href="#"><i
-                    class="fa-solid fa-location-dot me-2"></i>
+        <li class="sidebar-list-item py-2 border-top border-bottom"><a class="sidebar-link text-muted "
+                href="#"><i class="fa-solid fa-location-dot me-2"></i>
                 <span class="sidebar-link-title fs-5">Manage Tour </span></a>
         </li>
 
         {{-- Payroll --}}
-        <li class="sidebar-list-item py-2 border"><a class="sidebar-link text-muted " href="#"><i
-                    class="fa-solid fa-file-invoice-dollar me-2"></i>
+        <li class="sidebar-list-item py-2 border-top border-bottom"><a class="sidebar-link text-muted "
+                href="#"><i class="fa-solid fa-file-invoice-dollar me-2"></i>
                 <span class="sidebar-link-title fs-5">Payroll</span></a>
         </li>
 
         {{-- Attendance --}}
-        <li class="sidebar-list-item py-2 border"><a class="sidebar-link text-muted "
+        <li class="sidebar-list-item py-2 border-top border-bottom"><a class="sidebar-link text-muted "
                 href="{{ route('attendance.addAttendance') }}" data-bs-target="#tablesDropdown" role="button"
                 aria-expanded="false" data-bs-toggle="collapse"><i class="fa-regular fa-clock  me-2"></i>
                 <span class="sidebar-link-title fs-5">Attendance</span></a>
