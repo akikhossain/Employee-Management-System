c39ea3de9868894a2fa4529b8f797dc1cf9fee99
akik-hossain <akikhs00@gmail.com>
2023-11-26 19:10:37 +0600

    fixed single service page

--- resources/views/Frontend/pages/serviceSection/serviceDetails.blade.php
+++ resources/views/Frontend/pages/serviceSection/serviceDetails.blade.php
@@ -1,24 +1,16 @@
 @extends('Frontend.master')
 @section('content')
-<header class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="col-12">
-            <h1>Welcome to Blog Post!</h1>
-        </div>
-    </div>
-</header>
-<main class="container">
+
+<main class="container my-5">
+    <h1 class="text-center">Service Details</h1>
     <div class="row">
-        <div class="col-12">
+        <div class="col-12 mt-5">
             <article>
-                <h2>Posted on January 1, 2023 by Start Bootstrap</h2>
-                <h3>Web Design &middot; Freebies</h3>
-                @foreach ($services as $item)
+                {{-- <h2>Posted on January 1, 2023 by Start Bootstrap</h2> --}}
+                <h2>{{ $services->description }}</h2>
                 <img style="width: 900px; height: 400px;" src="{{ url('/uploads//' . $services->service_image) }}"
-                    alt="Blog post image">
-                <p>{{ $services->description }}</p>
-                <p>{{ $services->details }}</p>
-                @endforeach
+                    alt="service image" class="mb-5">
+                <h4 class="mt-4">{{ $services->details }}</h4>
             </article>
         </div>
     </div>
